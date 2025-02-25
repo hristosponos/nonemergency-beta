82dc47806566ecfa6528f496bb73f69d0a9694c4
Vadim Melnikov <vladmlnkv@icloud.com>
2025-02-26 01:19:26 +0200

    slider done

--- phone/slider.js
+++ phone/slider.js
@@ -1,12 +1,12 @@
 if (window.matchMedia("(max-width: 700px)").matches) {
   initalizedSwiper = true;
   const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
+    slidesPerView: "auto",
     centeredSlides: true,
     spaceBetween: 20, // Adjust for partial visibility
     loop: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: "#swiper-pagination",
       clickable: false,
     },
   });
