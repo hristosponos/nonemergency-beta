672932e90ceb24c7f957589793eb2cc9dfeb0771
Vadim Melnikov <vladmlnkv@icloud.com>
2025-02-25 18:54:00 +0200

    slider development

--- phone/slider.js
+++ phone/slider.js
@@ -1,11 +1,16 @@
+if (window.matchMedia("(max-width: 700px)").matches) {
+  initalizedSwiper = true;
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 20, // Adjust for partial visibility
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: false,
+    },
+  });
+}
 
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 20, // Adjust for partial visibility
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: false,
-  },
-});
+
+
