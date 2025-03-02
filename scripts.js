d99a2309147ad022c28e975a69746c1b95b0592b
Vadim Melnikov <vladmlnkv@icloud.com>
2025-03-02 23:29:14 +0200

    newsletter fix and mobile nav menu done

--- scripts.js
+++ scripts.js
@@ -1,59 +1,76 @@
 // RENDER FIRST AND LAST IMAGES ON SCREEN VERTICAL CENTER
 
-window.onload = function() {
-	var element = document.querySelector('.image-showcase > *:first-child');
-	var elementHeight = element.offsetHeight / 2; 
-	element.style.setProperty('--first-image-offset', `${elementHeight}px`);
-	
-	element = document.querySelector('.image-showcase > *:last-child');
-	elementHeight = element.offsetHeight / 2; 
-	element.style.setProperty('--last-image-offset', `${elementHeight}px`);
-}
+var first_element = document.querySelector('.image-showcase > *:first-child');
+var offset = first_element.offsetHeight / 2; 
+first_element.style.setProperty('--first-image-offset', `${offset}px`);
 
-// NEWSLETTER OPEN AND CLOSE FEATURES
+var last_element = document.querySelector('.image-showcase > *:last-child');
+offset = last_element.offsetHeight / 2; 
+last_element.style.setProperty('--last-image-offset', `${offset}px`);
 
-document.addEventListener("DOMContentLoaded", function () {
-    const openBtn = document.querySelectorAll(".newsletter-open");
-    const closeBtn = document.getElementById("newsletter-close");
-    const newsletter = document.getElementById("newsletter");
-    const overlay = document.getElementById("overlay");
+// MOBILE BURGER MENU OPEN
+
+const navigationOpen = document.getElementById("burger");
+const navigationClose = document.getElementById("mob-nav-close");
+const navigationWindow = document.getElementById("mob-nav-menu");
 
     // OPEN
 
-    openBtn.forEach(element => {
-      element.addEventListener("click", function() {
-        newsletter.style.display = "flex";
-      });
-    });
+navigationOpen.addEventListener("click", function() {
+    navigationWindow.classList.add('activated');
+});
 
     // CLOSE
-    closeBtn.addEventListener("click", function () {
-        newsletter.style.display = "none";
-    });
+navigationClose.addEventListener("click", function () {
+    navigationWindow.classList.remove('activated');
+});
 
-    overlay.addEventListener("click", function () {
-        newsletter.style.display = "none";
-    });
+// NEWSLETTER OPEN AND CLOSE FUNCTIONALTY
 
+const openBtn = document.querySelectorAll(".newsletter-open");
+const closeBtn = document.getElementById("newsletter-close");
+const newsletter = document.getElementById("newsletter");
+const overlay = document.getElementById("overlay");
+
+
+    // OPEN
+
+openBtn.forEach(element => {
+  element.addEventListener("click", function() {
+    newsletter.style.display = "block";
+
+    // CLOSE MOBILE NAVIGATION MENU, IF OPEN
+    if (navigationWindow.classList.contains("activated")){
+        navigationWindow.classList.remove('activated');
+    }
+  });
+});
+
+    // CLOSE
+
+closeBtn.addEventListener("click", function () {
+    newsletter.style.display = "none";
+});
+
+overlay.addEventListener("click", function () {
+    newsletter.style.display = "none";
 });
 
 // REDIRECT TO STRIPE
 
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("buy-product-form");
-    const sizeSelect = document.getElementById("size");
+const form = document.getElementById("buy-product-form");
+const sizeSelect = document.getElementById("size");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        return;
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    return;
 
-        const selectedSize = sizeSelect.value;
-        if (selectedSize=="not-selected") {
-            alert("Please choose a size to continue to checkout.");
-        } else if (selectedSize == "M"){
-            window.location.href = 'https://buy.stripe.com/test_00gbITbbR0Xv7hC7st';
-        } else if (selectedSize == "S"){
-            window.location.href = 'https://buy.stripe.com/test_3cs28j7ZFbC931m6oo';
-        }
-    });
+    const selectedSize = sizeSelect.value;
+    if (selectedSize=="not-selected") {
+        alert("Please choose a size to continue to checkout.");
+    } else if (selectedSize == "M"){
+        window.location.href = 'https://buy.stripe.com/test_00gbITbbR0Xv7hC7st';
+    } else if (selectedSize == "S"){
+        window.location.href = 'https://buy.stripe.com/test_3cs28j7ZFbC931m6oo';
+    }
 });
